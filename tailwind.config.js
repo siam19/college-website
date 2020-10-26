204762c103a58e6f15b5a4ac93ebf2634690332b
siam19 <48963312+siam19@users.noreply.github.com>
2020-10-26 16:50:10 +0600

    initial commit
    almost done. view tweakings needed

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  
+  purge: [],
+  theme: {
+    boxShadow: {
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1)',
+    },
+    extend: {
+      colors: {
+        'theme-black': '#030c11',
+        'theme-white': '#dde4f9',
+        'theme-blue': '#1c3c9b',
+        'theme-yellow': '#f6bd60',
+        'theme-darkblue': '#0c1941',
+      },
+      fontFamily: {
+        nunito: ['Nunito'],
+        playfair: ['Playfair Display'],
+      },
+      backgroundImage: theme => ({
+                 'slide1': "url('img/slide-1.png')" ,
+                 'slide2': "url('img/slide-2.png')",
+                 'slide3': "url('img/slide-3.png')",
+        }),
+    },
+    
+  },
+  variants: {
+    display: ['responsive', 'group-hover', 'group-focus'],
+    textColor: ['group-hover'],
+    fill: ['group-hover'],
+  },
+  plugins: [
+    
+  ],
+}
